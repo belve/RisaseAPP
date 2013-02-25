b5ebb6a5cca8cb993e373969418c0a6780b17023
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-02-25 17:53:49 +0100

    dd

--- odbctest.php
+++ odbctest.php
@@ -2,14 +2,24 @@
 
 <?php
 
-$server="SERVER";
-$user="edu";
-$password="admin";
-$database="Test";
 
 $conn=odbc_connect('risase','edu','admin');
+
+if (!$conn)
+  {exit("Connection Failed: " . $conn);}
+
+
 $sql="SELECT * FROM colores";
+
 $rs=odbc_exec($conn,$sql);
+if (!$rs)
+  {exit("Error in SQL");}
+
+
+print_r(odbc_fetch_row($rs));
+
+odbc_close($conn);
+
 
 ?>
 
