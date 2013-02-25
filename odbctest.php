a56c33e65d9c9136578dc65ab9e7980a19321d08
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-02-25 17:28:08 +0100

    rr

--- odbctest.php
+++ odbctest.php
@@ -1,8 +1,24 @@
+
+
 <?php
 
-echo "test";
+// El servidor con el formato: <computer>\<instance name> o 
+// <server>,<port> cuando se use un número de puerto diferente del de defecto
 
+$server = 'SERVER';
 
+// Connect to MSSQL
+$link = mssql_connect($server, 'edu', 'admin');
+
+if (!$link) {
+    die('Algo fue mal mientras se conectava a MSSQL');
+}else{
+	echo "yesss";
+}
 
 
 ?>
+
+
+
+
