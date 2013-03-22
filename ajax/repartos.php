54485cbddf479b4402ce57a5b5b4d97a61777d57
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-03-22 23:18:08 +0100

    ii

---
+++ ajax/repartos.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript" src="/jquery/jquery-1.9.0.min.js"></script>
+
+<link rel='stylesheet' type='text/css' href='/css/framework_inside.css' />
+
+
+
+
+</head>
+
+
+
+
+
+<body>
+
+<style>
+
+body {}
+table 	{border-collapse:collapse;  background-color: white;}
+tr		{height:20px;  }
+td		{width: 90px; border: 1px  solid #888888; margin:0px;}
+	
+</style>
+
+
+
+
+<table>
+
+
+<tr>
+<td style='border-bottom: 1px solid #dddddd;'>												<input type='text' class='camp_REP_art' value='1201188 / YUE -RJ-0096/1-PERRITO'></td>
+<td style='width:28px;border-bottom: 1px solid #dddddd;'>									<input type='text' class='camp_REP_rep' value=''></td>
+<td style='width:28px;border-bottom: 1px solid #dddddd;border-right:2px solid orange;'>		<input type='text' class='camp_REP_alm' value=''></td>
+
+
+
+
+<td style='width:28px;border-bottom: 1px solid #dddddd;'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px;border-bottom: 1px solid #dddddd;'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px;border-bottom: 1px solid #dddddd;'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px;border-bottom: 1px solid #dddddd;'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px;border-bottom: 1px solid #dddddd;'>									<input type='text' class='camp_REP_tie' value=''></td>
+
+
+</tr>
+
+
+
+
+
+
+<tr>
+<td style=''>											<input type='text' class='camp_REP_art' value='--- Alarma ---'></td>
+<td style='width:28px'>									<input type='text' class='camp_REP_rep' value=''></td>
+<td style='width:28px;border-right:2px solid orange'>	<input type='text' class='camp_REP_alm' value=''></td>
+
+
+
+
+<td style='width:28px'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px'>									<input type='text' class='camp_REP_tie' value=''></td>
+
+
+</tr>
+
+
+
+
+
+<tr>
+<td style='border-bottom: 1px solid #dddddd;'>												<input type='text' class='camp_REP_art' value='1201188 / YUE -RJ-0096/1-PERRITO'></td>
+<td style='width:28px;border-bottom: 1px solid #dddddd;'>									<input type='text' class='camp_REP_rep' value=''></td>
+<td style='width:28px;border-bottom: 1px solid #dddddd;border-right:2px solid orange;'>		<input type='text' class='camp_REP_alm' value=''></td>
+
+
+
+
+<td style='width:28px;border-bottom: 1px solid #dddddd;'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px;border-bottom: 1px solid #dddddd;'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px;border-bottom: 1px solid #dddddd;'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px;border-bottom: 1px solid #dddddd;'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px;border-bottom: 1px solid #dddddd;'>									<input type='text' class='camp_REP_tie' value=''></td>
+
+
+</tr>
+
+
+
+
+
+
+<tr>
+<td style=''>											<input type='text' class='camp_REP_art' value='--- Alarma ---'></td>
+<td style='width:28px'>									<input type='text' class='camp_REP_rep' value=''></td>
+<td style='width:28px;border-right:2px solid orange'>	<input type='text' class='camp_REP_alm' value=''></td>
+
+
+
+
+<td style='width:28px'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px'>									<input type='text' class='camp_REP_tie' value=''></td>
+<td style='width:28px'>									<input type='text' class='camp_REP_tie' value=''></td>
+
+
+</tr>
+
+</table>
+
+
+</body>
+</html>
