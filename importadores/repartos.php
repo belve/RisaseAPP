78ccc00d6f035f1c5da7799a5b4f2a1548c67a1a
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-03-22 20:09:03 +0100

    dd

---
+++ importadores/repartos.php
@@ -0,0 +1,93 @@
+<?php
+set_time_limit(0);
+foreach($_GET as $nombre_campo => $valor){  $asignacion = "\$" . $nombre_campo . "='" . $valor . "';";   eval($asignacion);};
+
+if(!$ini){$ini=0;};
+$fin=$ini+500;
+##### datos OLD
+$Ntab='Repartos';
+
+$Nid='art_idArticulo';
+
+$camp[1]='rep_NumeroRep';
+$camp[2]='rep_fecha';
+$camp[3]='rep_estado';
+
+
+
+
+##### datos NEW
+$nNtab="repartos";
+
+$nNid='id';
+
+
+$ncamp[1]='nomrep';
+$ncamp[2]='fecha';
+$ncamp[3]='estado';
+
+
+$conn=odbc_connect('risase','edu','admin');
+
+if (!$conn)
+  {exit("Connection Failed: " . $conn);}
+
+
+$sql="SELECT * FROM $Ntab where rep_fecha <= '31/12/2010' AND rep_fecha >= '01/01/2010';";
+
+
+$rs=odbc_exec($conn,$sql);
+if (!$rs)
+  {exit("Error in SQL");}
+
+
+
+
+while (odbc_fetch_row($rs))
+  {
+
+$count=1;
+foreach($camp as $nkey => $nomcampo){$count++;
+	 $valores[$count][$nkey]=trim(utf8_encode(odbc_result($rs,$nomcampo)));
+}
+
+
+  }
+
+odbc_close($conn);
+
+require_once("../db.php");
+
+
+$dbnivel=new DB('192.168.1.11','edu','admin','risase');
+if (!$dbnivel->open()){die($dbnivel->error());};
+
+print_r($valores);
+
+foreach ($valores as $val1 => $val2) {
+$nval1=$val2[1];$nval2=$val2[2];$nval3=$val2[3];
+
+$sqlcamps="";$sqlvals="";
+foreach ($val2 as $nnkey => $valuecamp)	{
+	
+	$sqlcamps .= "$ncamp[$nnkey],";
+	$sqlvals .= "'$valuecamp',";
+}
+	
+$sqlcamps=substr($sqlcamps, 0,strlen($sqlcamps)-1);	
+$sqlvals=substr($sqlvals, 0,strlen($sqlvals)-1);	
+	
+$queryp= "INSERT INTO $nNtab ($sqlcamps) values ($sqlvals);";
+#$dbnivel->query($queryp);
+}
+
+if (!$dbnivel->close()){die($dbnivel->error());};
+
+
+?>
+<!--
+<script>
+	 window.location.href = "/importadores/articulos.php?ini=<?php echo $fin;?>";
+</script>
+-->
+
