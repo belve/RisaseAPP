fa4c6a484032fffe790f91d75f7f800dd10f9a2e
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-08-05 15:58:54 +0200

    ww

--- importadores/detreparto.php
+++ importadores/detreparto.php
@@ -16,7 +16,7 @@ $queryp= "select id,id_tienda from tiendas";
 $dbnivel->query($queryp);
 while ($row = $dbnivel->fetchassoc()){$T[$row['id_tienda']]=$row['id'];};
 
-$queryp= "select id, nombre from agrupedidos where id > $ultREP ORDER BY id limit 10;";
+$queryp= "select id, nombre from agrupedidos where id > $ultREP AND tip=1 ORDER BY id limit 5;";
 $dbnivel->query($queryp);
 while ($row = $dbnivel->fetchassoc()){$cuales[$row['id']]=$row['nombre'];};
 
@@ -147,7 +147,7 @@ $dbnivel->query($queryp);
 
 if (!$dbnivel->close()){die($dbnivel->error());};
 
-$ultREP=$ultREP+10;
+$ultREP=$ultREP+5;
 ?>
 
 
