226cf0b3ef588f0e7d7b5c372d4878a06584bccc
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-08-06 15:09:57 +0200

    dd

---
+++ importadores/impstock.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$conn=odbc_connect('mdb','local','admin');
+
+if (!$conn)
+  {exit("Connection Failed: " . $conn);}
+
+
+$sql="SELECT art_idArticulo, art_UniStock, art_UniMini FROM Articulos;";
+
+
+$rs=odbc_exec($conn,$sql);
+if (!$rs)
+  {exit("Error in SQL");}
+
+
+
+$count=1;
+while (odbc_fetch_row($rs))
+  {$count++;
+
+	$id=trim(utf8_encode(odbc_result($rs,'art_idArticulo')));
+	echo $id;
+  }
+
+odbc_close($conn);
+
+
+?>
