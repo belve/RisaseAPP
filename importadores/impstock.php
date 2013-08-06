3dbee187e93504d65614aa29d3b7a3020ecdfb48
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-08-06 15:17:19 +0200

    ff

--- importadores/impstock.php
+++ importadores/impstock.php
@@ -7,7 +7,7 @@ if (!$conn)
   {exit("Connection Failed: " . $conn);}
 
 
-$sql="SELECT * FROM Articulos;";
+$sql="SELECT art_idArticulo, art_UniStock, art_UniMini FROM Articulos;";
 
 
 $rs=odbc_exec($conn,$sql);
@@ -16,15 +16,21 @@ if (!$rs)
 
 
 
-$count=1;
+$count=1;$values="";
 while (odbc_fetch_row($rs))
-  {$count++;
+  {
 
-	$id=trim(utf8_encode(odbc_result($rs,'art_idArticulo')));
-	echo $id;
+	$art_idArticulo=trim(utf8_encode(odbc_result($rs,'art_idArticulo')));
+	$art_UniStock=trim(utf8_encode(odbc_result($rs,'art_UniStock')));
+	$art_UniMini=trim(utf8_encode(odbc_result($rs,'art_UniMini')));
+	
+	$values .="('$art_idArticulo','$art_UniStock','$art_UniMini'),";
   }
 
 odbc_close($conn);
 
+$values=substr($values, 0,strlen($values)-1);	
 
+
+echo $values;
 ?>
