53911ea577d2f5c8ac1bf72010f0effa5b201066
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-09-11 23:54:07 +0200

    ddd

--- importadores/impstock2.php
+++ importadores/impstock2.php
@@ -13,7 +13,7 @@ if (!$conn)
 $sql="SELECT * FROM Articulos;";
 
 
-$rs=odbc_exec($conn,$sql);
+#$rs=odbc_exec($conn,$sql);
 if (!$rs)
   {exit("Error in SQL");}
 
@@ -38,8 +38,30 @@ require_once("../db.php");
 $dbnivelBAK=new DB('192.168.1.11','tpv','tpv','tpv_backup');
 if (!$dbnivelBAK->open()){die($dbnivelBAK->error());};
 
-$queryp= "INSERT INTO stocklocal_$idt (cod,stock,alarma) VALUES $values;";
+
+$chki=0;
+$queryp= "SHOW TABLES LIKE 'stocklocal_$idt';";
 $dbnivelBAK->query($queryp);
+while ($row = $dbnivelBAK->fetchassoc()){$chki=1;}
+
+if(!$chki){
+$queryp= "CREATE TABLE `stocklocal_$idt` (                        
+                 `id` bigint(255) unsigned NOT NULL AUTO_INCREMENT,  
+                 `cod` bigint(50) DEFAULT NULL,                      
+                 `stock` int(22) DEFAULT NULL,                       
+                 `alarma` int(22) DEFAULT NULL,                      
+                 `pvp` decimal(8,2) DEFAULT NULL,                    
+                 PRIMARY KEY (`id`),                                 
+                 KEY `cod` (`cod`)                                   
+               ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;";
+			   
+$dbnivelBAK->query($queryp);	
+}
+
+echo $chki;
+
+$queryp= "INSERT INTO stocklocal_$idt (cod,stock,alarma) VALUES $values;";
+#$dbnivelBAK->query($queryp);
 
 
 if (!$dbnivelBAK->close()){die($dbnivelBAK->error());};
