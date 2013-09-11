89313a02012fb1ed2b79ffaf5dd59e2e7fdeaa00
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-09-11 23:58:57 +0200

    ddd

--- variables.php
+++ variables.php
@@ -14,6 +14,7 @@ $tab_sync['colores']=1;
 $tab_sync['grupos']=1;
 $tab_sync['subgrupos']=1;
 $tab_sync['proveedores']=1;
+$tab_sync['tiendas']=1;
 
 global $dbnivel; global $tiendas;
 $dbnivel=new DB('192.168.1.11','edu','admin','risase');
