edf32d7c789e6fa34856fb77b18268943dcbf44c
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-09-12 21:36:26 +0200

    rrr

--- importadores/testodbc.php
+++ importadores/testodbc.php
@@ -1,10 +1,10 @@
 <?php
 
 
-$conn=odbc_connect("risasenew","remoto","azul88");
-if (!$conn)
-{exit("Connection Failed: " . $conn);}
-$sql="SELECT * FROM Articulos where art_idArticulo <= 50 ;";
+#$conn=odbc_connect("risasenew","remoto","azul88");
+#if (!$conn)
+#{exit("Connection Failed: " . $conn);}
+#$sql="SELECT * FROM Articulos where art_idArticulo <= 50 ;";
 
 #$rs=odbc_exec($conn,$sql);
 #if (!$rs)
@@ -21,6 +21,18 @@ $sql="SELECT * FROM Articulos where art_idArticulo <= 50 ;";
 #odbc_close($conn);
 
 
+ include('../adodb5/adodb.inc.php');
+$db =& ADONewConnection('odbc_mssql');
+$dsn = "Driver={SQL Server};Server=SERVER;Database=Risase;";
+$db->Connect($dsn,'remoto','azul88');
+
+$rs = $db->Execute('SELECT * FROM Articulos where art_idArticulo <= 50;');
+
+         print "<pre>";
+
+         print_r($rs->GetRows());
+
+         print "</pre>";
 
 ?>
 
