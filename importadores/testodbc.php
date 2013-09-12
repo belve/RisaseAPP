539d19d395ad6a1ad601714f2e3922e98e310291
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-09-12 19:55:04 +0200

    dd

---
+++ importadores/testodbc.php
@@ -0,0 +1,37 @@
+<?php
+
+
+$conn=odbc_connect("risasenew","remoto","azul88");
+
+if (!$conn)
+  {exit("Connection Failed: " . $conn);}
+
+
+$sql="SELECT * FROM Articulos where art_idArticulo <= 50 ;";
+
+
+$rs=odbc_exec($conn,$sql);
+if (!$rs)
+  {exit("Error in SQL");}
+
+
+
+
+while (odbc_fetch_row($rs))
+  {
+
+
+foreach($camp as $nkey => $nomcampo){
+	 $valores[trim(odbc_result($rs,$Nid))][$nkey]=trim(utf8_encode(odbc_result($rs,$nomcampo)));
+}
+
+
+  }
+
+odbc_close($conn);
+
+
+
+print_r($valores);
+?>
+
