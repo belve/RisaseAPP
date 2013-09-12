409775ca56429b815ca5c05e1d0c3cee068c2391
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-09-12 20:07:25 +0200

    dd

--- importadores/testodbc.php
+++ importadores/testodbc.php
@@ -1,34 +1,35 @@
 <?php
 
 
-$conn=odbc_connect("risasenew","remoto","azul88");
-
-if (!$conn)
-  {exit("Connection Failed: " . $conn);}
-
-
+#$conn=odbc_connect("risasenew","remoto","azul88");
+#if (!$conn)
+#{exit("Connection Failed: " . $conn);}
 $sql="SELECT * FROM Articulos where art_idArticulo <= 50 ;";
-
-
-$rs=odbc_exec($conn,$sql);
-if (!$rs)
-  {exit("Error in SQL");}
+#$rs=odbc_exec($conn,$sql);
+#if (!$rs)
+#  {exit("Error in SQL");}
 
 
 
 
-while (odbc_fetch_row($rs))
-  {
+#while (odbc_fetch_row($rs)){
+#foreach($camp as $nkey => $nomcampo){
+#$valores[trim(odbc_result($rs,$Nid))][$nkey]=trim(utf8_encode(odbc_result($rs,$nomcampo)));
+#}}
+
+#odbc_close($conn);
 
 
-foreach($camp as $nkey => $nomcampo){
-	 $valores[trim(odbc_result($rs,$Nid))][$nkey]=trim(utf8_encode(odbc_result($rs,$nomcampo)));
-}
 
+$msconnect = @mssql_connect("SERVER","remoto","azul88") 
+or die("Couldn't connect to SQL Server on ");
+$msdb = @mssql_select_db("Risase",$msconnect)
+or die("Couldn't connect to SQL BD");
 
-  }
-
-odbc_close($conn);
+if (!$msconnect) {echo "NO"; }
+ 
+$result = mssql_query($sql);
+while ($valores = mssql_fetch_array($result)) {};
 
 
 
