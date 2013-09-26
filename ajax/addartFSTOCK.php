f22368368cd0ad32d590864ab6e2506ca0cf98ab
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-09-26 16:00:56 +0200

    cc

---
+++ ajax/addartFSTOCK.php
@@ -0,0 +1,45 @@
+<?php
+$id_proveedor="";$id_grupo="";$id_subgrupo="";$id_color="";$codigo="";$pvp="";$desde="";$hasta="";$temporada="";$hago="";
+$yalistados="";
+$detalles="";
+$comentarios="";
+$ord=1;
+$tab=1;
+$arts=array();
+$vals=array();
+	
+foreach($_GET as $nombre_campo => $valor){  $asignacion = "\$" . $nombre_campo . "='" . $valor . "';";   eval($asignacion);};
+require_once("../db.php");
+require_once("../variables.php");
+
+
+if (!$dbnivel->open()){die($dbnivel->error());};
+
+
+$options="";$forsync="";
+require_once("../functions/listador.php");
+    if($yalistados){$options .=" OR id IN ($yalistados)"; };
+	
+	$queryp= "select * from articulos where $options;"; 
+	
+	$dbnivel->query($queryp);
+	while ($row = $dbnivel->fetchassoc()){
+	$cd=$row['codbarras']; $id_articulo=$row['id']; $pvp=$row['pvp']; $pvori=$row['pvp'];
+	
+	$g=substr($cd,0,1); $s=substr($cd,1,1);$c=substr($cd,4);
+		
+	$arts[$g][$s][$c][$id_articulo]=$cd;	
+	}
+
+if (!$dbnivel->close()){die($dbnivel->error());};
+
+if(count($arts)>0){
+ksort($arts);
+foreach ($arts as $g => $sub) {ksort($sub); foreach ($sub as $s => $cods) {ksort($cods); foreach ($cods as $co => $art) { foreach ($art as $id => $codbarras) {
+	$vals[$id]=$codbarras;
+}}}}}
+
+echo json_encode($vals);
+
+
+?>
---
+++ ajax/insFIJstock.php
@@ -0,0 +1,35 @@
+<?php
+
+$arts=array();
+$tsel=array();
+
+$arts=$_GET['arts'];
+$tsel=$_GET['tsel'];
+
+
+require_once("../db.php");
+require_once("../variables.php");
+
+if(count($tsel)>0){if(count($arts)>0){
+$queryp="INSERT INTO fij_stock (id_tienda,id_articulo,fijo,suma) VALUES ";	
+	
+foreach ($tsel as $key => $idt) { foreach ($arts as $ida => $vals) {
+$fijo=$vals['f'];		
+$suma=$vals['s'];
+$queryp .="($idt,$ida,'$fijo','$suma'),";	
+}}	
+	
+$queryp=substr($queryp,0,-1);
+$queryp .=";";
+
+
+if (!$dbnivel->open()){die($dbnivel->error());};
+	$dbnivel->query($queryp); echo $queryp;
+if (!$dbnivel->close()){die($dbnivel->error());};	
+
+}}
+
+
+	
+
+?>
