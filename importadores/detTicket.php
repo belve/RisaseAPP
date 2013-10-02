b4e8002b272f1faa079d988558ad17dbaf67edb8
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-10-02 15:37:02 +0200

    ww

--- importadores/detTicket.php
+++ importadores/detTicket.php
@@ -11,16 +11,17 @@ while ($row = $dbnivel->fetchassoc()){$idt=$row['midt'];};
 if(!$idt){$idt="0";};
 
 $cnoms="";
-$queryp= "select id_ticket from tickets WHERE id > $idt LIMIT 10;";
+$queryp= "select id, id_ticket from tickets WHERE id > $idt LIMIT 10;";
 $dbnivel->query($queryp); 
-while ($row = $dbnivel->fetchassoc()){$cnoms.="'" . $row['id_ticket'] . "',";};
+while ($row = $dbnivel->fetchassoc()){
+	$cnoms.="'" . $row['id_ticket'] . "',";
+	$idsti[$row['id_ticket']]=$row['id'];
+};
 $cnoms=substr($cnoms, 0,-1);
 
-
-echo $cnoms;
-
-
 if (!$dbnivel->close()){die($dbnivel->error());};
+
+
 
 
 
@@ -29,9 +30,9 @@ $db =& ADONewConnection($driv);
 $dsn = "Driver={SQL Server};Server=SERVER;Database=Risase;";
 $db->Connect($dsn,'remoto','azul88');
 $db->debug = false;
-$sql="SELECT det_idarticulo, det_cantidad, det_precio FROM DetalleTicket where det_idTicket IN ($cnoms);";
+$sql="SELECT det_idTicket, det_idarticulo, det_cantidad, det_precio FROM DetalleTicket where det_idTicket IN ($cnoms);";
 $rs = $db->Execute($sql);
-echo $sql;
+
 
 $rows = $rs->GetRows();
 
@@ -40,11 +41,13 @@ $rows = $rs->GetRows();
 $vals="";
 if(count($rows)>0){
 foreach ($rows as $key => $row) {
-	
-	
+
+$detalles[$row[0]][idt]=$idsti[$row[0]];	
+$detalles[$row[0]][ida]=$row[1];
+$detalles[$row[0]][can]=$row[2];
+$detalles[$row[0]][pre]=$row[3];	
 }}
 
-print_r($rows);
-
+print_r($detalles);
 
 ?>
