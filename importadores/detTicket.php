cbd61d0e53e0574a6a50b2cb64755eb80d86ade9
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-10-02 16:10:10 +0200

    ee

--- importadores/detTicket.php
+++ importadores/detTicket.php
@@ -11,11 +11,14 @@ while ($row = $dbnivel->fetchassoc()){$idt=$row['midt'];};
 if(!$idt){$idt="0";};
 
 $cnoms="";
-$queryp= "select id, id_ticket from tickets WHERE id > $idt LIMIT 10;";
+$queryp= "select id, id_ticket, id_tienda, fecha, hora from tickets WHERE id > $idt LIMIT 10;";
 $dbnivel->query($queryp); 
 while ($row = $dbnivel->fetchassoc()){
 	$cnoms.="'" . $row['id_ticket'] . "',";
 	$idsti[$row['id_ticket']]=$row['id'];
+	$ids_ti[$row['id']]=$row['id_tienda'];
+	$ids_fech[$row['id']]=$row['fecha'];
+	$ids_hora[$row['id']]=$row['hora'];
 };
 $cnoms=substr($cnoms, 0,-1);
 
@@ -36,9 +39,9 @@ $rs = $db->Execute($sql);
 
 $rows = $rs->GetRows();
 
+$db->Close();
 
-
-$vals="";
+$vals="";$idsbusco=array();
 if(count($rows)>0){
 foreach ($rows as $key => $row) {
 
@@ -46,9 +49,43 @@ $tt=trim($row[0]);
 $detalles[$tt]['idt']=$idsti[$tt];	
 $detalles[$tt]['ida']=$row[1];
 $detalles[$tt]['can']=$row[2];
-$detalles[$tt]['pre']=$row[3];	
+$detalles[$tt]['pre']=$row[3];
+
+if(!array_key_exists($row[1],$idsbusco)){$idsbusco[$row[1]]=1;};
+	
 }}
 
-print_r($detalles);
+$idas="";
+foreach ($idsbusco as $idb => $poin) {
+$idas.="$idb,";	
+}
+$idas=substr($idas, 0,-1);
+if (!$dbnivel->open()){die($dbnivel->error());};
+
+
+$queryp= "select id, codbarras from articulos where id IN ($idas);";
+$dbnivel->query($queryp);
+while ($row = $dbnivel->fetchassoc()){$cdbars[$row['id']]=$row['codbarras'];};
+
+
+foreach ($detalles as $tnom => $values) {
+$idti=$values['idt'];
+$id_ti=$ids_ti[$idti];		
+$codbar=$cdbars[$values['ida']]; $g=substr($codbar,0,1);$sg=substr($codbar,1,1);
+$can=$values['can'];
+$pre=$values['pre'];
+
+$fecha=$ids_fech[$idti];	
+$hora=$ids_hora[$idti];	
+
+
+$insV .="('$idti','$id_ti','$tnom','$codbar',$g,$sg,'$can','$pre','$fecha','$hora'),";	
+}
+$insV=substr($insV, 0,-1);
+
+$queryp="(idt,id_tienda,id_ticket,id_articulo,g,sg,cantidad,importe,fecha,hora) VALUES $insV;";
+$dbnivel->query($queryp);
+
+if (!$dbnivel->close()){die($dbnivel->error());};
 
 ?>
