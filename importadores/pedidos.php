d9fc23bedb5c3918af5d7bc0e1d6e2c41f533a8f
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-10-16 10:34:51 +0200

    www

--- importadores/pedidos.php
+++ importadores/pedidos.php
@@ -1,5 +1,5 @@
 <?php 
-$ini=2008;
+$ini=2009;
 set_time_limit(0);
 foreach($_GET as $nombre_campo => $valor){  $asignacion = "\$" . $nombre_campo . "='" . $valor . "';";   eval($asignacion);};
 
@@ -94,7 +94,7 @@ $ini++;
 ?>
 
 <script>
-	// window.location.href = "/importadores/pedidos.php?ini=<?php echo $ini;?>";
+	 window.location.href = "/importadores/pedidos.php?ini=<?php echo $ini;?>";
 </script>
 
 
