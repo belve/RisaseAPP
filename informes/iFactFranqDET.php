3dc4ebfc31ef0f042884fe83718ce83a54939b90
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-11-08 10:58:37 +0100

    sss

--- informes/iFactFranqDET.php
+++ informes/iFactFranqDET.php
@@ -13,6 +13,10 @@ $debug=0;
 $eqtip[1]="REPARTOS";
 $eqtip[2]="PEDIDOS";
 
+$eqtp[1]="R:";
+$eqtp[2]="P:";
+
+
 $frqcia="";
 $options=""; $cong=0;
 $id_proveedor="";$id_grupo="";$id_subgrupo="";$id_color="";$codigo="";$pvp="";$desde="";$hasta="";$temporada="";$hago="";
@@ -68,7 +72,15 @@ $datos[$row['id_tienda']][$row['tip']][$row['cb']]['impt']=$row['impt'];
 if (!$dbnivel->close()){die($dbnivel->error());};
 
 
+$cols[1]['B']='B';
+$cols[1]['C']='C';
+$cols[1]['D']='D';
+$cols[1]['E']='E';
 
+$cols[2]['B']='G';
+$cols[2]['C']='H';
+$cols[2]['D']='I';
+$cols[2]['E']='J';
 
 $grid=array();
 $anchos=array();
@@ -78,58 +90,76 @@ $Mrang=array();
 $BTrang=array();$p=1;$sumas=array();
 
 
-$fila=1; $lT=""; $lTi="";
+$fila=1; $lT=""; $lTi=""; $count=1;
 
 
 foreach ($datos as $idt => $tips){
-$totQ=0; $totI=0;
-####### cabecera tienda		
+$totQ=0; $totI=0; $cc=1;
+####### cabecera tienda	
+	
 $grid[$fila]['B']=$tiendasN[$idt];	
-$Mrang["B$fila:E$fila"]=1;
-$align["B$fila"]='C';
-$BOLDrang	['B' . $fila]=1;
-$fila++;$fila++;		
+$Mrang[$cols[1]['B'] . $fila . ":" . $cols[2]['E'] . $fila]=1;
+$align[$cols[$cc]['B'] . $fila]='C';
+
+$BOLDrang	[$cols[$cc]['B'] . $fila]=1;
+ $ffila=$fila+3; $fila++;$fila++;	$count=1;
  foreach ($tips as $tip => $cbs) {
 	############ cabecera tipo	 		
-	$grid[$fila]['B']=$eqtip[$tip];	
-	$Mrang["B$fila:E$fila"]=1;
-	$align["B$fila"]='C';
-	$BOLDrang	['B' . $fila]=1;
-	$fila++;	 	
-	 	 foreach ($cbs as $cb => $vals) { 
+	$grid[$fila][$cols[$cc]['B']]=$eqtip[$tip];	
+	$Mrang[$cols[$cc]['B'] . $fila . ":" . $cols[$cc]['E'] . $fila]=1;
+	$align[$cols[$cc]['B'] . $fila ]='C';
+	$BOLDrang	[$cols[$cc]['B'] . $fila ]=1;
+	$fila++; $count++;	 	
 
-			$grid[$fila]['B']=$cb;
-			$grid[$fila]['C']=$vals['qty'];	$totQ=$totQ+$vals['qty'];	
+	foreach ($cbs as $cb => $vals) {
+	
+	 if($count>40){
+		if($cc==1){$cc=2; $fila=$ffila; $count=1; }else{$cc=1; $paginas[$fila]=1; $fila++; $count=1; $ffila=$fila; };			  
+	  }
 			
-			$grid[$fila]['D']=number_format($vals['impu'],2,',','.');
-			$align["D$fila"]='R';
 			
-			$grid[$fila]['E']=number_format($vals['impt'],2,',','.');	$totI=$totI+$vals['impt'];
-			$align["E$fila"]='R';
+			$grid[$fila][$cols[$cc]['B']]=$eqtp[$tip] . $cb;
+			$grid[$fila][$cols[$cc]['C']]=$vals['qty'];	$totQ=$totQ+$vals['qty'];	
 			
-			$BOLDrang	['B' . $fila . ':' . 'E' . $fila]=2;
+			$grid[$fila][$cols[$cc]['D']]=number_format($vals['impu'],2,',','.');
+			$align[$cols[$cc]['D'] . $fila]='R';
 			
-			$fila++;
+			$grid[$fila][$cols[$cc]['E']]=number_format($vals['impt'],2,',','.');	$totI=$totI+$vals['impt'];
+			$align[$cols[$cc]['E'] . $fila]='R';
+			
+			$BOLDrang	[$cols[$cc]['B'] . $fila . ":" . $cols[$cc]['E'] . $fila]=2;
+			
+			$fila++;$count++;
 	
 }}
 
-$fila++;
-$grid[$fila]['B']='TOTAL:';
-$grid[$fila]['C']=$totQ;							$align["C$fila"]='R';
-$grid[$fila]['E']=number_format($totI,2,',','.');	$align["E$fila"]='R';
-$BOLDrang	['B' . $fila . ':' . 'E' . $fila]=1;
+$fila++;$count++;
+$fila=$ffila+40;
+$grid[$fila][$cols[$cc]['B']]='TOTAL:';
+$grid[$fila][$cols[$cc]['C']]=$totQ;							$align[$cols[$cc]['C'] . $fila]='R';
+$grid[$fila][$cols[$cc]['E']]=number_format($totI,2,',','.');	$align[$cols[$cc]['E'] . $fila]='R';
+$BOLDrang	[$cols[$cc]['B'] . $fila . ":" . $cols[$cc]['E'] . $fila]=1;
 $paginas[$fila]=1;
-$fila++;
+$fila++;$count++;
 }
 
 $anchos['A']=1;
 
-$anchos['B']=15;
-$anchos['C']=6;
+$anchos['B']=18;
+$anchos['C']=9;
 $anchos['D']=12;
 $anchos['E']=12;
 
 $anchos['F']=5;
+
+$anchos['G']=18;
+$anchos['H']=9;
+$anchos['I']=12;
+$anchos['J']=12;
+
+
+
+
 
 if(count($grid)>0){
 
