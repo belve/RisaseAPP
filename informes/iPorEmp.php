cfc11e757fe4bcb9b46072ee4d5469d40d82b9fc
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-11-11 10:05:31 +0100

    www

--- informes/iPorEmp.php
+++ informes/iPorEmp.php
@@ -184,7 +184,7 @@ $_SESSION['Mrang']=$Mrang;
 $_SESSION['BTrang']=$BTrang;
 $_SESSION['format']=$format;
 $_SESSION['paginas']=$paginas;
-$_SESSION['nomfil']="PorDias-" . $mes;
+$_SESSION['nomfil']="PorEmpleado" . $mes;
 $_SESSION['BOLDrang']=$BOLDrang;
 $res['ng']=count($grid)+count($anchos)+count($align)+count($crang)+count($Mrang)+count($BTrang)+count($paginas)+count($format);
 
