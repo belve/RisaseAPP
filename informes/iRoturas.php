b3f9d559544c54323b3de6017902ef8be4f12074
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-11-14 19:16:33 +0100

    jjj

--- informes/iRoturas.php
+++ informes/iRoturas.php
@@ -246,7 +246,7 @@ $_SESSION['Mrang']=$Mrang;
 $_SESSION['BTrang']=$BTrang;
 $_SESSION['format']=$format;
 $_SESSION['paginas']=$paginas;
-$_SESSION['nomfil']="Inventario-" . date('Y') . date('m') . date('d');
+$_SESSION['nomfil']="Devoluciones-" . date('Y') . date('m') . date('d');
 
 $res['ng']=count($grid)+count($anchos)+count($align)+count($crang)+count($Mrang)+count($BTrang)+count($paginas)+count($format);
 
