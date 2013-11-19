09a1760af14e32fd7cdd6feefa8562c74237c682
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-11-19 10:27:57 +0100

    yyy

--- logged.php
+++ logged.php
@@ -119,7 +119,7 @@ $(window).keydown(function(evt) {
     	<ul class="submenu">
         	<li onclick="javascript:owin('v_45','Procesar Risasa','');"><a>Procesar Risasa</a></li>
         	<li onclick="javascript:owin('v_48','Eliminador de referencias','');"><a>Eliminador de referencias</a></li>
-        
+        	<li onclick="javascript:owin('v_49','Borrar rebajas','');"><a>Borrar rebajas</a></li>
         </ul>
     	
     </li>
